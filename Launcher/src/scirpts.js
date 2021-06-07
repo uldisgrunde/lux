97e4b70a2cf96ad4b4110c34901ba8f9a9a4f048
Uldis Grunde-Zeiferts <uldis.grunde@gmail.com>
2021-06-07 23:08:48 +0300

    Reorganizacija 1

--- Launcher/src/scirpts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".access-QR").click(function () {
-        $("#viewer").attr(".ralfs/BARCODE");
-    });
-
-    $(".access-Barcode").click(function () {
-        $("#viewer").attr(".localhost/ralfs/QR");
-    });
-});
